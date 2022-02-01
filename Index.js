dfa1f6bd607f1999e930565687708fe9b19c11c7
hnahTC <81008164+CThanh1111@users.noreply.github.com>
2022-02-01 21:20:02 +0700

    Update Index.js
    clean...

--- Index.js
+++ Index.js
@@ -20,121 +20,124 @@ let username = "WAT"
 let ver = "1.12.2" 
 
 const fs = require('fs');
-function genRandomInt (integer) {
-  return Math.floor(Math.random()*integer)
+// function genRandomInt (integer) {
+//   return Math.floor(Math.random()*integer)
 
+// }
+function genRandomInt (min, max) {
+  return Math.floor(Math.random()*max) + min
 }
 function createBot () {
-  let a  = genRandomInt(10)
-  let b = genRandomInt(10)
-  let c = genRandomInt(10)
-  let d = genRandomInt(10)
-  function reverseTheNumber(temp) {
-    var ans = 0;
-    while (temp > 0) {
-        var rem = temp % 10;
-        ans = ans * 10 + rem;
-        temp = parseInt(temp / 10);
-    }
-    return ans;
-  }
+  var a = genRandomInt(1, 9)
+  var b = genRandomInt(1, 9)
+  var c = genRandomInt(1, 9)
+  var d = genRandomInt(1, 9)
+//   function reverseTheNumber(temp) {
+//     var ans = 0;
+//     while (temp > 0) {
+//         var rem = temp % 10;
+//         ans = ans * 10 + rem;
+//         temp = parseInt(temp / 10);
+//     }
+//     return ans;
+//   }
 
-  function convert0To5(a)
-  {
-    if (a == 0)
-    return 1;
+//   function convert0To5(a)
+//   {
+//     if (a == 0)
+//     return 1;
 
-    else {
-      var temp = 0;
+//     else {
+//       var temp = 0;
 
-      while (a > 0) {
-          var digit = a % 10;
-
-
-          if (digit == 0)
-              digit = 5;
-
-          temp = temp * 10 + digit;
-          a = parseInt(a / 10);
-      }
+//       while (a > 0) {
+//           var digit = a % 10;
 
 
-      return reverseTheNumber(temp);
-  }
-  }
-  function convert0To4(b)
-  {
-    if (b == 0)
-    return 1;
+//           if (digit == 0)
+//               digit = 5;
 
-    else {
-      var temp = 0;
-
-      while (b > 0) {
-          var digit = b % 10;
+//           temp = temp * 10 + digit;
+//           a = parseInt(a / 10);
+//       }
 
 
-          if (digit == 0)
-              digit = 5;
+//       return reverseTheNumber(temp);
+//   }
+//   }
+//   function convert0To4(b)
+//   {
+//     if (b == 0)
+//     return 1;
 
-          temp = temp * 10 + digit;
-          b = parseInt(b / 10);
-      }
+//     else {
+//       var temp = 0;
 
-
-      return reverseTheNumber(temp);
-    }
-  }
-  function convert0To3(c)
-  {
-    if (c == 0)
-    return 1;
-
-    else {
-      var temp = 0;
-
-      while (c > 0) {
-          var digit = c % 10;
-
-        
-          if (digit == 0)
-              digit = 5;
-
-          temp = temp * 10 + digit;
-          c = parseInt(c / 10);
-      }
+//       while (b > 0) {
+//           var digit = b % 10;
 
 
-      return reverseTheNumber(temp);
-    }
-  }
-  function convert0To2(d)
-  {
-    if (d == 0)
-    return 1;
+//           if (digit == 0)
+//               digit = 5;
 
-    else {
-      var temp = 0;
+//           temp = temp * 10 + digit;
+//           b = parseInt(b / 10);
+//       }
 
-      while (d > 0) {
-          var digit = d % 10;
+
+//       return reverseTheNumber(temp);
+//     }
+//   }
+//   function convert0To3(c)
+//   {
+//     if (c == 0)
+//     return 1;
+
+//     else {
+//       var temp = 0;
+
+//       while (c > 0) {
+//           var digit = c % 10;
 
         
-          if (digit == 0)
-              digit = 5;
+//           if (digit == 0)
+//               digit = 5;
 
-          temp = temp * 10 + digit;
-          d = parseInt(d / 10);
-      }
+//           temp = temp * 10 + digit;
+//           c = parseInt(c / 10);
+//       }
 
 
-      return reverseTheNumber(temp);
-    }
-  }
-  let z = convert0To5(a);
-  let x = convert0To4(b);
-  let v = convert0To3(c);
-  let f = convert0To2(d);
+//       return reverseTheNumber(temp);
+//     }
+//   }
+//   function convert0To2(d)
+//   {
+//     if (d == 0)
+//     return 1;
+
+//     else {
+//       var temp = 0;
+
+//       while (d > 0) {
+//           var digit = d % 10;
+
+        
+//           if (digit == 0)
+//               digit = 5;
+
+//           temp = temp * 10 + digit;
+//           d = parseInt(d / 10);
+//       }
+
+
+//       return reverseTheNumber(temp);
+//     }
+//   }
+//   let z = convert0To5(a);
+//   let x = convert0To4(b);
+//   let v = convert0To3(c);
+//   let f = convert0To2(d);
 
   var bot = mineflayer.createBot({
     host: ip ,
@@ -144,20 +147,24 @@ function createBot () {
 
   bot.on('windowOpen', (window)=> {
     window.requiresConfirmation = false;
-    bot.clickWindow(z, 0, 0)
-    bot.clickWindow(x, 0, 0)
-    bot.clickWindow(v, 0, 0)
-    bot.clickWindow(f, 0, 0);
+//     bot.clickWindow(z, 0, 0)
+//     bot.clickWindow(x, 0, 0)
+//     bot.clickWindow(v, 0, 0)
+//     bot.clickWindow(f, 0, 0);
+    bot.clickWindow(a, 0, 0)
+    bot.clickWindow(b, 0, 0)
+    bot.clickWindow(c, 0, 0)
+    bot.clickWindow(d, 0, 0);
 
     setTimeout(() => {bot.chat('/2y2c')}, 15 * 1000)
     setTimeout(() => { bot.clickWindow(10, 0, 0) }, 20 * 1000)
   }) 
   process.setMaxListeners(0);
 
-  console.log(z)
-  console.log(x)
-  console.log(v)
-  console.log(f)
+  console.log(a)
+  console.log(b)
+  console.log(c)
+  console.log(d)
 
 
 
@@ -165,7 +172,7 @@ function createBot () {
     let channel = client.channels.cache.get(`854712390817808404`)
     console.log(`nice ${username}`);
     bot.once('spawn', () => { 
-        channel.send(`EZ PIN ${z} ${x} ${v} ${f} (ingame: ${username})`)
+        channel.send(`EZ PIN ${a} ${b} ${c} ${d} (ingame: ${username})`)
       })
     })
   bot.on('message', message => {
